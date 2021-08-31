3a1e0da30509229dbe1165cceae69dac8aa46478
nuitnuit <junlinlee0@gmail.com>
2021-08-31 22:52:23 +0800

    Toggle buttons datasets

--- src/components/CollectedDatasets.js
+++ src/components/CollectedDatasets.js
@@ -1,11 +1,41 @@
 
 import React from "react"
+import Container from "react-bootstrap/Container";
 import '../App.css';
+import tableIcon from "../assets/table.png";
+import graphIcon from "../assets/graph.png";
+import piechartIcon from "../assets/piechart.png";
+import women from "../data/women.csv";
 
 function CollectedDatasets() {
     return (
         <div className="CollectedDatasets">
-            <h1>This is Collected Datasets page</h1>
+            <Container>
+                <br />
+                <div class="d-flex justify-content-center">
+                    <h1>Welcome To SnekStats</h1>
+                </div>
+                <div class="row align-items-center my-5">
+                    <br />
+                    <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                        <label class="btn btn-secondary active">
+                            <input type="radio" name="viewType" id="option1" autocomplete="off" checked/> 
+                            <img src={tableIcon} width="50" height="50"></img>
+                        </label>
+                        <label class="btn btn-secondary">
+                            <input type="radio" name="viewType" id="option2" autocomplete="off"/>
+                            <img src={graphIcon} width="50" height="50"></img>
+                        </label>
+                        <label class="btn btn-secondary">
+                            <input type="radio" name="viewType" id="option2" autocomplete="off"/>
+                            <img src={piechartIcon} width="50" height="50"></img>
+                        </label>
+                    </div>
+                    <div class="col-md-12">
+                        <h1>CONTENT HERE PLS THX</h1>
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 }
